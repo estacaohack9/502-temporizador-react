f6ab131327f09e30f36044b4ffdf3ff3b2eb980d
Renan Rodrigo <renanrb@ime.usp.br>
2018-10-09 11:41:45 -0300

    Adicionar primeira versao dos arquivos

---
+++ src/AppStyle.js
@@ -0,0 +1,35 @@
+const AppStyle = {
+  titulo:{
+    color: 'red',
+    fontSize: '46pt',
+    fontFamily: 'Comic Sans MS, sans serif',
+    margin: 0
+  },
+  divDeFora:{
+    height: '60vh',
+    width: '100vw',
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    flexDirection: 'column',
+    textAlign: 'center'
+  },
+  relogio:{
+    fontSize: '60pt',
+  },
+  botao:{
+    width: 100,
+    height: 100,
+    borderRadius: '50%',
+    border: '1px dotted black',
+    color: 'white',
+    backgroundColor: 'red',
+    fontSize: '1.4em',
+    margin: '0 10px'
+  },
+  botoes:{
+    marginTop: 50
+  },
+}
+
+export default AppStyle;
